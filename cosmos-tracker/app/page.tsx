292dbe51852c1b0c93ec0590be5bb9fd36a9692d
Sai <sailakshmy94@gmail.com>
2026-05-24 19:38:55 +0530

    Move HomeScreen into screens folder

--- cosmos-tracker/app/page.tsx
+++ cosmos-tracker/app/page.tsx
@@ -1,4 +1,4 @@
-import HomeScreen from "./components/HomeScreen";
+import HomeScreen from "./screens/HomeScreen";
 
 export default async function Home() {
   let apod;
--- cosmos-tracker/app/screens/HomeScreen.tsx
+++ cosmos-tracker/app/screens/HomeScreen.tsx
@@ -1,7 +1,6 @@
 "use client";
 import Image from "next/image";
-import React from "react";
-import { Video } from "./Video";
+import { Video } from "../components/Video";
 
 interface HomeProps {
   title: string;
